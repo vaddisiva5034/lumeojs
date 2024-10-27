45121a9006bcc01dd0e97370f16b91ec00c26ce8
Kiran Vaddi <vaddisiva5034@gmail.com>
2024-10-28 00:26:14 +0530

    added the build

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: './lumeo.js', // Adjust path accordingly
+    output: {
+        filename: 'lumeo.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'Lumeo', // This makes it available as a global variable
+        libraryTarget: 'umd',
+    },
+    mode: 'production', // Use 'development' for debugging
+};
